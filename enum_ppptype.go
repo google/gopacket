b4c6d591ea4d4f0d731aa893576f4bc01a2a2835
Graeme Connell <gconnell@google.com>
2012-11-28 00:40:45 -0700

    PPP

---
+++ enum_ppptype.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2012 Graeme Connell. All rights reserved.
+// Copyright (c) 2009-2012 Andreas Krennmair. All rights reserved.
+
+package gopacket
+
+import (
+	"errors"
+)
+
+type PppType uint16
+
+const (
+	PPP_IP4 PppType = 0x0021
+	PPP_IP6 PppType = 0x0057
+)
+
+func (p PppType) decode(data []byte, s *specificLayers) (out decodeResult) {
+	switch p {
+	case PPP_IP4:
+		return decodeIp4(data, s)
+	case PPP_IP6:
+		return decodeIp6(data, s)
+	}
+	out.err = errors.New("Unsupported PPP type")
+	return
+}
+
+func (p PppType) Decode(data []byte, lazy DecodeMethod) Packet {
+	return newPacket(data, lazy, p)
+}
---
+++ layer_ppp.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2012 Graeme Connell. All rights reserved.
+// Copyright (c) 2009-2012 Andreas Krennmair. All rights reserved.
+
+package gopacket
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+type PPP struct {
+	PppType PppType
+}
+
+type pppAddress []byte
+
+var singletonPppAddress = pppAddress{}
+
+func (p pppAddress) String() string {
+	return "point"
+}
+func (p pppAddress) Raw() []byte {
+	return p
+}
+
+func (p *PPP) SrcLinkAddr() Address {
+	return singletonPppAddress
+}
+func (p *PPP) DstLinkAddr() Address {
+	return singletonPppAddress
+}
+
+func (p *PPP) LayerType() LayerType {
+	return TYPE_PPP
+}
+
+var decodePpp decoderFunc = func(data []byte, s *specificLayers) (out decodeResult) {
+	ppp := &PPP{}
+	if data[0]&0x1 == 0 {
+		if data[1]&0x1 == 0 {
+			out.err = errors.New("PPP has invalid type")
+			return
+		}
+		ppp.PppType = PppType(binary.BigEndian.Uint16(data[:2]))
+		out.left = data[2:]
+	} else {
+		ppp.PppType = PppType(data[0])
+		out.left = data[1:]
+	}
+	out.layer = ppp
+	out.next = ppp.PppType
+	s.link = ppp
+	return
+}
