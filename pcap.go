53fd12508901b3fd2c50e4508e892ab2c52ce2a0
Graeme Connell <gconnell@google.com>
2012-12-10 19:05:40 -0700

    Fix up naming and speed up packet copy.

--- pcap.go
+++ pcap.go
@@ -51,8 +51,8 @@ func (p *PcapHandle) Next() (pkt *Packet) {
 	return rv
 }
 
-// OpenLive opens a device and returns a *PcapHandle
-func OpenLive(device string, snaplen int32, promisc bool, timeout_ms int32) (handle *PcapHandle, err error) {
+// OpenLivePcap opens a device and returns a *PcapHandle
+func OpenLivePcap(device string, snaplen int32, promisc bool, timeout_ms int32) (handle *PcapHandle, err error) {
 	var buf *C.char
 	buf = (*C.char)(C.calloc(ERRBUF_SIZE, 1))
 	h := new(PcapHandle)
@@ -75,7 +75,8 @@ func OpenLive(device string, snaplen int32, promisc bool, timeout_ms int32) (han
 	return
 }
 
-func OpenOffline(file string) (handle *PcapHandle, err error) {
+// OpenOfflinePcap opens a file and returns its contents as a *PcapHandle
+func OpenOfflinePcap(file string) (handle *PcapHandle, err error) {
 	var buf *C.char
 	buf = (*C.char)(C.calloc(ERRBUF_SIZE, 1))
 	h := new(PcapHandle)
@@ -94,16 +95,24 @@ func OpenOffline(file string) (handle *PcapHandle, err error) {
 	return
 }
 
-func (p *PcapHandle) NextEx() (pkt *Packet, result int32) {
-	var pkthdr_ptr *C.struct_pcap_pkthdr
-	var pkthdr C.struct_pcap_pkthdr
+type PcapResultCode int32
+
+const (
+  PcapResultOk PcapResultCode = 1
+	PcapResultTimeoutExpired PcapResultCode = 0
+	PcapResultReadError = -1
+	PcapResultNoMorePackets = -2
+)
+
+
+func (p *PcapHandle) NextEx() (pkt *Packet, result PcapResultCode) {
+	var pkthdr *C.struct_pcap_pkthdr
 
 	var buf_ptr *C.u_char
 	var buf unsafe.Pointer
-	result = int32(C.hack_pcap_next_ex(p.cptr, &pkthdr_ptr, &buf_ptr))
+	result = PcapResultCode(C.hack_pcap_next_ex(p.cptr, &pkthdr, &buf_ptr))
 
 	buf = unsafe.Pointer(buf_ptr)
-	pkthdr = *pkthdr_ptr
 
 	if nil == buf {
 		return
@@ -112,11 +121,7 @@ func (p *PcapHandle) NextEx() (pkt *Packet, result int32) {
 	pkt.Time = time.Unix(int64(pkthdr.ts.tv_sec), int64(pkthdr.ts.tv_usec))
 	pkt.Caplen = uint32(pkthdr.caplen)
 	pkt.Len = uint32(pkthdr.len)
-	pkt.Data = make([]byte, pkthdr.caplen)
-
-	for i := uint32(0); i < pkt.Caplen; i++ {
-		pkt.Data[i] = *(*byte)(unsafe.Pointer(uintptr(buf) + uintptr(i)))
-	}
+	pkt.Data = C.GoBytes(buf, C.int(pcthdr.caplen))
 	return
 }
 
@@ -162,32 +167,18 @@ func Version() string {
 	return C.GoString(C.pcap_lib_version())
 }
 
-func (p *PcapHandle) DataLink() LinkType {
+func (p *PcapHandle) LinkType() LinkType {
 	return LinkType(C.pcap_datalink(p.cptr))
 }
 
-func (p *PcapHandle) SetDatalink(dlt LinkType) error {
+func (p *PcapHandle) SetLinkType(dlt LinkType) error {
 	if -1 == C.pcap_set_datalink(p.cptr, C.int(dlt)) {
 		return p.GetError()
 	}
 	return nil
 }
 
-func DatalinkValueToName(dlt int) string {
-	if name := C.pcap_datalink_val_to_name(C.int(dlt)); name != nil {
-		return C.GoString(name)
-	}
-	return ""
-}
-
-func DatalinkValueToDescription(dlt int) string {
-	if desc := C.pcap_datalink_val_to_description(C.int(dlt)); desc != nil {
-		return C.GoString(desc)
-	}
-	return ""
-}
-
-func Findalldevs() (ifs []PcapInterface, err error) {
+func FindAllDevs() (ifs []PcapInterface, err error) {
 	var buf *C.char
 	buf = (*C.char)(C.calloc(ERRBUF_SIZE, 1))
 	defer C.free(unsafe.Pointer(buf))
