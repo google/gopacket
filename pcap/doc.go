4a5895b11ea9d48de021629ded33347937a3110e
Graeme Connell <gconnell@google.com>
2012-12-12 12:56:57 -0700

    Add more doc comments for pcap.

--- pcap/doc.go
+++ pcap/doc.go
@@ -8,10 +8,10 @@ Reading PCAP Files
 
 The following code can be used to read in data from a pcap file.
 
- if h, err := pcap.OpenOffline(filename); err != nil {
+ if handle, err := pcap.OpenOffline(filename); err != nil {
    panic(err)
  } else {
-   for packet, err := h.Next(); err != pcap.NextErrorNoMorePackets; packet, err = h.Next() {
+   for packet, err := handle.Next(); err != pcap.NextErrorNoMorePackets; packet, err = handle.Next() {
      if err != nil {
        fmt.Println("Error reading in packet:", err)
      } else {
@@ -25,21 +25,36 @@ Reading Live Packets
 The following code can be used to read in data from a live device, in this case
 "eth0".
 
- var handle *pcap.Handle
- if h, err := pcap.OpenLive("eth0", 1600, true, 0); err != nil {
+ if handle, err := pcap.OpenLive("eth0", 1600, true, 0); err != nil {
    panic(err)
- } else if err := h.SetFilter("tcp and port 80"); err != nil {
+ } else if err := handle.SetFilter("tcp and port 80"); err != nil {
    panic(err)
  } else {
-   handle = h
- }
- for {
-   packet, err := handle.Next()
-   if err != nil {
-     fmt.Println("Error reading in packet:", err)
-     continue
+   for {
+     packet, err := handle.Next()
+     if err != nil {
+       fmt.Println("Error reading in packet:", err)
+       continue
+     }
+     handlePacket(packet)
    }
-   handlePacket(packet)
  }
+
+Changing Packet Decoding Behavior
+
+You have a few options for changing the behavior of packet decoding on a handle.
+The first is to modify the DecodeOptions of the handle:
+
+ handle, _ := pcap.OpenLive(...)
+ // See gopacket.DecodeOptions for more options you can set here.
+ handle.DecodeOptions.Lazy = true
+
+The second option is to change the default decoder used to decode each packet.
+By default, we set the decoder based on the link type of the handler, so you
+should very rarely need to do this.  However, if you want, you can:
+
+ handle, _ := pcap.OpenLive(...)
+ // Force the handle to decode every packet as if it were PPP
+ handle.Decoder = gopacket.LinkTypePPP
 */
 package pcap
