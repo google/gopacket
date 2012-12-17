1be7ee692e3866fc4f2cab3bb2b8f12692ae55fa
Graeme Connell <gconnell@google.com>
2012-12-17 15:17:49 -0700

    Generate new macs.

--- valid_mac_prefixes.go
+++ valid_mac_prefixes.go
@@ -3,7 +3,7 @@
 package gopacket
 
 // Created by gopacket_mac_fetcher, don't edit manually
-// Generated at 2012-12-17 10:07:38.233595 -0700 MST
+// Generated at 2012-12-17 15:17:26.767668 -0700 MST
 // Fetched from "http://standards.ieee.org/develop/regauth/oui/oui.txt"
 
 // ValidMACPrefixMap maps a valid MAC address prefix to the name of the
@@ -13235,6 +13235,7 @@ var validMACPrefixMap = map[[3]byte]string{
 	[3]byte{24, 72, 216}:   "Fastback Networks",
 	[3]byte{24, 78, 148}:   "MESSOA TECHNOLOGIES INC.",
 	[3]byte{24, 82, 83}:    "Pixord Corporation",
+	[3]byte{24, 83, 224}:   "Hanyang Digitech Co.Ltd",
 	[3]byte{24, 85, 15}:    "Cisco SPVTG",
 	[3]byte{24, 89, 51}:    "Cisco SPVTG",
 	[3]byte{24, 102, 227}:  "Veros Systems, Inc.",
@@ -13529,6 +13530,7 @@ var validMACPrefixMap = map[[3]byte]string{
 	[3]byte{40, 186, 24}:   "NextNav, LLC",
 	[3]byte{40, 190, 155}:  "Technicolor USA Inc.",
 	[3]byte{40, 192, 218}:  "Juniper Networks",
+	[3]byte{40, 198, 142}:  "NETGEAR INC.,",
 	[3]byte{40, 199, 24}:   "Altierre",
 	[3]byte{40, 201, 20}:   "Taimag Corporation",
 	[3]byte{40, 204, 1}:    "Samsung Electronics Co.,Ltd",
