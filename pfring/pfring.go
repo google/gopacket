1ad77ff3db198dd0d23b77cdcaef02abaab44dc5
Graeme Connell <gconnell@google.com>
2012-12-17 09:41:12 -0700

    Add lots of comments, and speed up Inject.
    Inject is sped up by avoiding a spurious data copy.

--- pfring/pfring.go
+++ pfring/pfring.go
@@ -43,6 +43,7 @@ const (
 
 // NewRing creates a new PFRing.  Note that when the ring is initially created,
 // it is disabled.  The caller must call Enable to start receiving packets.
+// The caller should call Close on the given ring when finished with it.
 func NewRing(device string, snaplen uint32, flags Flag) (ring *Ring, _ error) {
 	dev := C.CString(device)
 	defer C.free(unsafe.Pointer(dev))
@@ -56,6 +57,8 @@ func NewRing(device string, snaplen uint32, flags Flag) (ring *Ring, _ error) {
 	return
 }
 
+// Close closes the given Ring.  After this call, the Ring should no longer be
+// used.
 func (r *Ring) Close() {
 	C.pfring_close(r.cptr)
 }
@@ -108,6 +111,8 @@ const (
 	ClusterPerFlowTCP5Tuple ClusterType = C.cluster_per_flow_tcp_5_tuple
 )
 
+// SetCluster sets which cluster the ring should be part of, and the cluster
+// type to use.
 func (r *Ring) SetCluster(cluster int, typ ClusterType) error {
 	if rv := C.pfring_set_cluster(r.cptr, C.u_int(cluster), C.cluster_type(typ)); rv != 0 {
 		return fmt.Errorf("Unable to set cluster, got error code %d", rv)
@@ -115,6 +120,8 @@ func (r *Ring) SetCluster(cluster int, typ ClusterType) error {
 	return nil
 }
 
+// RemoveFromCluster removes the ring from the cluster it was put in with
+// SetCluster.
 func (r *Ring) RemoveFromCluster() error {
 	if rv := C.pfring_remove_from_cluster(r.cptr); rv != 0 {
 		return fmt.Errorf("Unable to remove from cluster, got error code %d", rv)
@@ -122,6 +129,7 @@ func (r *Ring) RemoveFromCluster() error {
 	return nil
 }
 
+// SetSamplingRate sets the sampling rate to 1/<rate>.
 func (r *Ring) SetSamplingRate(rate int) error {
 	if rv := C.pfring_set_sampling_rate(r.cptr, C.u_int32_t(rate)); rv != 0 {
 		return fmt.Errorf("Unable to set sampling rate, got error code %d", rv)
@@ -129,6 +137,7 @@ func (r *Ring) SetSamplingRate(rate int) error {
 	return nil
 }
 
+// SetBPFFilter sets the BPF filter for the ring.
 func (r *Ring) SetBPFFilter(bpf_filter string) error {
 	filter := C.CString(bpf_filter)
 	defer C.free(unsafe.Pointer(filter))
@@ -138,6 +147,7 @@ func (r *Ring) SetBPFFilter(bpf_filter string) error {
 	return nil
 }
 
+// RemoveBPFFilter removes the BPF filter from the ring.
 func (r *Ring) RemoveBPFFilter() error {
 	if rv := C.pfring_remove_bpf_filter(r.cptr); rv != 0 {
 		return fmt.Errorf("Unable to remove BPF filter, got error code %d", rv)
@@ -145,16 +155,17 @@ func (r *Ring) RemoveBPFFilter() error {
 	return nil
 }
 
+// Inject uses the ring to send raw packet data to the interface.
 func (r *Ring) Inject(data []byte) error {
-	buf := C.CString(string(data))
-	defer C.free(unsafe.Pointer(buf))
-
+	buf := (*C.char)(unsafe.Pointer(&data[0]))
 	if rv := C.pfring_send(r.cptr, buf, C.u_int(len(data)), 1); rv != 0 {
 		return fmt.Errorf("Unable to send packet data, got error code %d", rv)
 	}
 	return nil
 }
 
+// Enable enables the given ring.  This function MUST be called on each new
+// ring after it has been set up, or that ring will NOT receive packets.
 func (r *Ring) Enable() error {
 	if rv := C.pfring_enable_ring(r.cptr); rv != 0 {
 		return fmt.Errorf("Unable to enable ring, got error code %d", rv)
@@ -162,6 +173,8 @@ func (r *Ring) Enable() error {
 	return nil
 }
 
+// Disable disables the given ring.  After this call, it will no longer receive
+// packets.
 func (r *Ring) Disable() error {
 	if rv := C.pfring_disable_ring(r.cptr); rv != 0 {
 		return fmt.Errorf("Unable to disable ring, got error code %d", rv)
@@ -173,6 +186,7 @@ type Stats struct {
 	Received, Dropped uint64
 }
 
+// Stats returns statistsics for the ring.
 func (r *Ring) Stats() (s Stats, err error) {
 	var stats C.pfring_stat
 	if rv := C.pfring_stats(r.cptr, &stats); rv != 0 {
@@ -187,11 +201,18 @@ func (r *Ring) Stats() (s Stats, err error) {
 type Direction C.packet_direction
 
 const (
-	TXOnly  Direction = C.tx_only_direction
-	RXOnly  Direction = C.rx_only_direction
-	RXAndTX Direction = C.rx_and_tx_direction
+	// TransmitOnly will only capture packets transmitted by the ring's
+	// interface(s).
+	TransmitOnly Direction = C.tx_only_direction
+	// ReceiveOnly will only capture packets received by the ring's
+	// interface(s).
+	ReceiveOnly Direction = C.rx_only_direction
+	// ReceiveAndTransmit will capture both received and transmitted packets on
+	// the ring's interface(s).
+	ReceiveAndTransmit Direction = C.rx_and_tx_direction
 )
 
+// SetDirection sets which packets should be captured by the ring.
 func (r *Ring) SetDirection(d Direction) error {
 	if rv := C.pfring_set_direction(r.cptr, C.packet_direction(d)); rv != 0 {
 		return fmt.Errorf("Unable to set ring direction, got error code %d", rv)
@@ -202,11 +223,16 @@ func (r *Ring) SetDirection(d Direction) error {
 type SocketMode C.socket_mode
 
 const (
-	SendOnly       SocketMode = C.send_only_mode
-	ReceiveOnly    SocketMode = C.recv_only_mode
+	// SendOnly sets up the ring to only send packets (Inject), not read them.
+	SendOnly SocketMode = C.send_only_mode
+	// ReceiveOnly sets up the ring to only receive packets (NextPacketData), not
+	// send them.
+	ReceiveOnly SocketMode = C.recv_only_mode
+	// SendAndReceive sets up the ring to both send and receive packets.
 	SendAndReceive SocketMode = C.send_and_recv_mode
 )
 
+// SetSocketMode sets the mode of the ring socket to send, receive, or both.
 func (r *Ring) SetSocketMode(s SocketMode) error {
 	if rv := C.pfring_set_socket_mode(r.cptr, C.socket_mode(s)); rv != 0 {
 		return fmt.Errorf("Unable to set socket mode, got error code %d", rv)
@@ -214,6 +240,9 @@ func (r *Ring) SetSocketMode(s SocketMode) error {
 	return nil
 }
 
+// SetApplicationName sets a string name to the ring.  This name is available in
+// /proc stats for pf_ring.  By default, NewRing automatically calls this with
+// argv[0].
 func (r *Ring) SetApplicationName(name string) error {
 	buf := C.CString(name)
 	defer C.free(unsafe.Pointer(buf))
