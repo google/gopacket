054c7f5937e806736018ea31ebd07f3688bca4e1
Graeme Connell <gconnell@google.com>
2012-12-29 20:47:31 -0700

    Make FDDI a link layer.

--- layers/fddi.go
+++ layers/fddi.go
@@ -11,19 +11,24 @@ type FDDI struct {
 	baseLayer
 	FrameControl   FDDIFrameControl
 	Priority       uint8
-	SrcMAC, DstMac []byte
+	SrcMAC, DstMAC []byte
 }
 
 func (f *FDDI) LayerType() gopacket.LayerType { return LayerTypeFDDI }
+
+func (f *FDDI) LinkFlow() gopacket.Flow {
+	return gopacket.NewFlow(EndpointMAC, f.SrcMAC, f.DstMAC)
+}
 
 func decodeFDDI(data []byte, p gopacket.PacketBuilder) error {
 	f := &FDDI{
 		FrameControl: FDDIFrameControl(data[0] & 0xF8),
 		Priority:     data[0] & 0x07,
 		SrcMAC:       data[1:7],
-		DstMac:       data[7:13],
+		DstMAC:       data[7:13],
 		baseLayer:    baseLayer{data[:13], data[13:]},
 	}
+	p.SetLinkLayer(f)
 	p.AddLayer(f)
 	return p.NextDecoder(f.FrameControl)
 }
