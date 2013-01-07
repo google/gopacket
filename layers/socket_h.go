771575758de8f5138009261fd356713f88340fea
Graeme Connell <gconnell@google.com>
2013-01-07 12:00:46 -0700

    Locking down licensing.
    Got some final licensing advice from an uber-open-sourcer, and updated licensing
    information accordingly across the board.
    
    Also, fixed issue with loopback INET6, where different unix distros have
    different PF_INET6 values (wtf?).

--- layers/socket_h.go
+++ layers/socket_h.go
@@ -1,4 +1,8 @@
 // Copyright 2012 Google, Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
 
 package layers
 
@@ -7,6 +11,10 @@ import "C"
 
 const (
 	ProtocolFamilyIPv4 ProtocolFamily = C.PF_INET
-	ProtocolFamilyIPv6 ProtocolFamily = C.PF_INET6
-	ProtocolFamilyPPP  ProtocolFamily = C.PF_PPP
+	// BSDs use different values for INET6... glory be.  These values taken from
+	// tcpdump 4.3.0.
+	ProtocolFamilyIPv6BSD     ProtocolFamily = 24
+	ProtocolFamilyIPv6FreeBSD ProtocolFamily = 28
+	ProtocolFamilyIPv6Darwin  ProtocolFamily = 30
+	ProtocolFamilyIPv6Linux   ProtocolFamily = 10
 )
