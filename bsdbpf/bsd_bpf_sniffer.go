f67b95de5159b03662c400f5fb7898298fab9006
David Stainton <dstainton415@gmail.com>
2015-06-17 12:22:35 -0700

    copyright lines first

--- bsdbpf/bsd_bpf_sniffer.go
+++ bsdbpf/bsd_bpf_sniffer.go
@@ -1,10 +1,10 @@
-// +build darwin dragonfly freebsd netbsd openbsd
-
 // Copyright 2012 Google, Inc. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file in the root of the source
 // tree.
+
+// +build darwin dragonfly freebsd netbsd openbsd
 
 package bsdbpf
 
