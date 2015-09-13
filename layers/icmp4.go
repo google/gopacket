23570b2b226e6d2d46e95966ba5c25f33ca645ab
René Scheibe <rene.scheibe@gmail.com>
2015-09-13 12:38:31 +0200

    reordering code
    * to be in line with layers/icmp6.go

--- layers/icmp4.go
+++ layers/icmp4.go
@@ -14,8 +14,6 @@ import (
 	"strconv"
 )
 
-type ICMPv4TypeCode uint16
-
 const (
 	ICMPv4TypeEchoReply              = 0
 	ICMPv4TypeDestinationUnreachable = 3
@@ -34,70 +32,7 @@ const (
 	ICMPv4TypeAddressMaskReply       = 18
 )
 
-// ICMPv4 is the layer for IPv4 ICMP packet data.
-type ICMPv4 struct {
-	BaseLayer
-	TypeCode ICMPv4TypeCode
-	Checksum uint16
-	Id       uint16
-	Seq      uint16
-}
-
-// LayerType returns gopacket.LayerTypeICMPv4
-func (i *ICMPv4) LayerType() gopacket.LayerType { return LayerTypeICMPv4 }
-
-func decodeICMPv4(data []byte, p gopacket.PacketBuilder) error {
-	i := &ICMPv4{}
-	err := i.DecodeFromBytes(data, p)
-	if err != nil {
-		return err
-	}
-	p.AddLayer(i)
-	return p.NextDecoder(gopacket.LayerTypePayload)
-}
-
-var tooShort error = fmt.Errorf("icmp layer less than 8 bytes")
-
-func (i *ICMPv4) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
-	if len(data) < 8 {
-		df.SetTruncated()
-		return tooShort
-	}
-	i.TypeCode = ICMPv4TypeCode(binary.BigEndian.Uint16(data[:2]))
-	i.Checksum = binary.BigEndian.Uint16(data[2:4])
-	i.Id = binary.BigEndian.Uint16(data[4:6])
-	i.Seq = binary.BigEndian.Uint16(data[6:8])
-	i.BaseLayer = BaseLayer{data[:8], data[8:]}
-	return nil
-}
-
-// SerializeTo writes the serialized form of this layer into the
-// SerializationBuffer, implementing gopacket.SerializableLayer.
-// See the docs for gopacket.SerializableLayer for more info.
-func (i *ICMPv4) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) error {
-	bytes, err := b.PrependBytes(8)
-	if err != nil {
-		return err
-	}
-	binary.BigEndian.PutUint16(bytes, uint16(i.TypeCode))
-	binary.BigEndian.PutUint16(bytes[4:], i.Id)
-	binary.BigEndian.PutUint16(bytes[6:], i.Seq)
-	if opts.ComputeChecksums {
-		bytes[2] = 0
-		bytes[3] = 0
-		i.Checksum = tcpipChecksum(b.Bytes(), 0)
-	}
-	binary.BigEndian.PutUint16(bytes[2:], i.Checksum)
-	return nil
-}
-
-func (i *ICMPv4) CanDecode() gopacket.LayerClass {
-	return LayerTypeICMPv4
-}
-
-func (i *ICMPv4) NextLayerType() gopacket.LayerType {
-	return gopacket.LayerTypePayload
-}
+type ICMPv4TypeCode uint16
 
 func (a ICMPv4TypeCode) String() string {
 	typ := uint8(a >> 8)
@@ -199,4 +134,69 @@ func (a ICMPv4TypeCode) String() string {
 		codeStr = strconv.Itoa(int(code))
 	}
 	return fmt.Sprintf("%s(%s)", typeStr, codeStr)
+}
+
+// ICMPv4 is the layer for IPv4 ICMP packet data.
+type ICMPv4 struct {
+	BaseLayer
+	TypeCode ICMPv4TypeCode
+	Checksum uint16
+	Id       uint16
+	Seq      uint16
+}
+
+// LayerType returns gopacket.LayerTypeICMPv4
+func (i *ICMPv4) LayerType() gopacket.LayerType { return LayerTypeICMPv4 }
+
+var tooShort error = fmt.Errorf("icmp layer less than 8 bytes")
+
+func (i *ICMPv4) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
+	if len(data) < 8 {
+		df.SetTruncated()
+		return tooShort
+	}
+	i.TypeCode = ICMPv4TypeCode(binary.BigEndian.Uint16(data[:2]))
+	i.Checksum = binary.BigEndian.Uint16(data[2:4])
+	i.Id = binary.BigEndian.Uint16(data[4:6])
+	i.Seq = binary.BigEndian.Uint16(data[6:8])
+	i.BaseLayer = BaseLayer{data[:8], data[8:]}
+	return nil
+}
+
+// SerializeTo writes the serialized form of this layer into the
+// SerializationBuffer, implementing gopacket.SerializableLayer.
+// See the docs for gopacket.SerializableLayer for more info.
+func (i *ICMPv4) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) error {
+	bytes, err := b.PrependBytes(8)
+	if err != nil {
+		return err
+	}
+	binary.BigEndian.PutUint16(bytes, uint16(i.TypeCode))
+	binary.BigEndian.PutUint16(bytes[4:], i.Id)
+	binary.BigEndian.PutUint16(bytes[6:], i.Seq)
+	if opts.ComputeChecksums {
+		bytes[2] = 0
+		bytes[3] = 0
+		i.Checksum = tcpipChecksum(b.Bytes(), 0)
+	}
+	binary.BigEndian.PutUint16(bytes[2:], i.Checksum)
+	return nil
+}
+
+func (i *ICMPv4) CanDecode() gopacket.LayerClass {
+	return LayerTypeICMPv4
+}
+
+func (i *ICMPv4) NextLayerType() gopacket.LayerType {
+	return gopacket.LayerTypePayload
+}
+
+func decodeICMPv4(data []byte, p gopacket.PacketBuilder) error {
+	i := &ICMPv4{}
+	err := i.DecodeFromBytes(data, p)
+	if err != nil {
+		return err
+	}
+	p.AddLayer(i)
+	return p.NextDecoder(gopacket.LayerTypePayload)
 }
