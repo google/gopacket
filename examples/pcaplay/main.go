ca9ece108292518f0e18348999c72e7048610eaa
Laurent Hausermann <laurent.hausermann@gmail.com>
2015-12-29 17:26:45 +0100

    Integrate some Sentryo team patches :  - add nice stats about the number of packet sent, and remaining time  - add a fast mode, to replay packet as fast as possible

--- examples/pcaplay/main.go
+++ examples/pcaplay/main.go
@@ -24,28 +24,74 @@ import (
 
 var iface = flag.String("i", "eth0", "Interface to write packets to")
 var fname = flag.String("r", "", "Filename to read from")
+var fast = flag.Bool("f", false, "Send each packets as fast as possible")
 
 var lastTS time.Time
 var lastSend time.Time
 
-func writePacket(handle *pcap.Handle, buf []byte, ci gopacket.CaptureInfo) {
+var start time.Time
+var bytesSent int
+
+func writePacketDelayed(handle *pcap.Handle, buf []byte, ci gopacket.CaptureInfo) {
 	if ci.CaptureLength != ci.Length {
 		// do not write truncated packets
 		return
 	}
 
 	intervalInCapture := ci.Timestamp.Sub(lastTS)
-	elapsedTime := time.Now().Sub(lastSend)
+	elapsedTime := time.Since(lastSend)
 
 	if (intervalInCapture > elapsedTime) && !lastSend.IsZero() {
 		time.Sleep(intervalInCapture - elapsedTime)
 	}
 
 	lastSend = time.Now()
-	if err := handle.WritePacketData(buf); err != nil {
-		log.Fatal(err)
-	}
+	writePacket(handle, buf)
 	lastTS = ci.Timestamp
+}
+
+func writePacket(handle *pcap.Handle, buf []byte) error {
+	if err := handle.WritePacketData(buf); err != nil {
+		log.Printf("Failed to send packet: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func pcapInfo(filename string) (start time.Time, end time.Time, packets int, size int) {
+	handleRead, err := pcap.OpenOffline(*fname)
+	if err != nil {
+		log.Fatal("PCAP OpenOffline error (handle to read packet):", err)
+	}
+
+	var previousTs time.Time
+	var deltaTotal time.Duration
+
+	for {
+		data, ci, err := handleRead.ReadPacketData()
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		} else if err == io.EOF {
+			break
+		} else {
+
+			if start.IsZero() {
+				start = ci.Timestamp
+			}
+			end = ci.Timestamp
+			packets++
+			size += len(data)
+
+			if previousTs.IsZero() {
+				previousTs = ci.Timestamp
+			} else {
+				deltaTotal += ci.Timestamp.Sub(previousTs)
+				previousTs = ci.Timestamp
+			}
+		}
+	}
+	fmt.Printf("Avg packet rate %d/s\n", packets/int(deltaTotal.Seconds()))
+	return start, end, packets, size
 }
 
 func main() {
@@ -76,16 +122,37 @@ func main() {
 	}
 	defer handleWrite.Close()
 
+	start = time.Now()
+	pkt := 0
+	tsStart, tsEnd, packets, size := pcapInfo(*fname)
+
 	// Loop over packets and write them
 	for {
 		data, ci, err := handleRead.ReadPacketData()
 		switch {
 		case err == io.EOF:
+			fmt.Printf("\nFinished in %s", time.Since(start))
 			return
 		case err != nil:
-			log.Fatal(err)
+			log.Printf("Failed to read packet %d: %s\n", pkt, err)
 		default:
-			writePacket(handleWrite, data, ci)
+			if *fast {
+				writePacket(handleWrite, data)
+			} else {
+				writePacketDelayed(handleWrite, data, ci)
+			}
+
+			bytesSent += len(data)
+			duration := time.Since(start)
+			pkt++
+
+			if duration > time.Second {
+				rate := bytesSent / int(duration.Seconds())
+				remainingTime := tsEnd.Sub(tsStart) - duration
+				fmt.Printf("\rrate %d kB/sec - sent %d/%d kB - %d/%d packets - remaining time %s",
+					rate/1000, bytesSent/1000, size/1000,
+					pkt, packets, remainingTime)
+			}
 		}
 	}
 
