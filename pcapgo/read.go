6d329c16bfb0d3e2aafb4c2956950bee3a0a8906
David Stainton <dstainton415@gmail.com>
2016-03-04 22:15:03 +0100

    Revert "pcapgo returns error for malformed packets"
    This reverts commit 6cc846c3f25434a8484d0c75ab01878b27efa10e.

--- pcapgo/read.go
+++ pcapgo/read.go
@@ -102,7 +102,6 @@ func (r *Reader) ReadPacketData() (data []byte, ci gopacket.CaptureInfo, err err
 
 	var n int
 	if 16+ci.CaptureLength > len(r.buf) {
-		err = gopacket.ErrMalformedPacket
 		return
 	}
 	data = r.buf[16 : 16+ci.CaptureLength]
