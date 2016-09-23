ac56fbb2a782cce23207e35942a6b19a4bfaea9f
Graeme Connell <gconnell@google.com>
2016-09-23 10:46:35 -0600

    Fix copyright and package comment in ip4defrag.

--- ip4defrag/defrag_test.go
+++ ip4defrag/defrag_test.go
@@ -1,4 +1,9 @@
 // Copyright 2013 Google, Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
+
 package ip4defrag
 
 import (
