a38ec04fc5828383dea85a8cd53d006699aea11c
Graeme Connell <gconnell@google.com>
2016-09-23 11:06:40 -0600

    Golint for pcapgo.

--- .travis.golint.sh
+++ .travis.golint.sh
@@ -3,7 +3,7 @@
 cd "$(dirname $0)"
 
 go get github.com/golang/lint/golint
-DIRS=". tcpassembly ip4defrag macs"
+DIRS=". tcpassembly ip4defrag macs pcapgo"
 # Add subdirectories here as we clean up golint on each.
 for subdir in $DIRS; do
   pushd $subdir
