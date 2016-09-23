9145ba3734b71ac76e035e7fe2af7fbed513e1fd
Graeme Connell <gconnell@google.com>
2016-09-23 11:08:42 -0600

    Run golint on pcap, ignoring CannotSetRfMon error.
    We can't rename an exported error value, but golint wants us to.  Ignore that
    line.

--- .travis.golint.sh
+++ .travis.golint.sh
@@ -3,11 +3,11 @@
 cd "$(dirname $0)"
 
 go get github.com/golang/lint/golint
-DIRS=". tcpassembly ip4defrag macs pcapgo"
+DIRS=". tcpassembly ip4defrag macs pcapgo pcap"
 # Add subdirectories here as we clean up golint on each.
 for subdir in $DIRS; do
   pushd $subdir
-  if golint | grep .; then
+  if golint | grep -v CannotSetRFMon | grep .; then
     exit 1
   fi
   popd
