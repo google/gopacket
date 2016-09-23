7036c04887bd89897dddd39483a2d9d313eee937
Graeme Connell <gconnell@google.com>
2016-09-23 11:17:04 -0600

    Golint for tcpassembly/tcpreader.

--- .travis.golint.sh
+++ .travis.golint.sh
@@ -3,11 +3,14 @@
 cd "$(dirname $0)"
 
 go get github.com/golang/lint/golint
-DIRS=". tcpassembly ip4defrag macs pcapgo pcap"
+DIRS=". tcpassembly tcpassembly/tcpreader ip4defrag macs pcapgo pcap"
 # Add subdirectories here as we clean up golint on each.
 for subdir in $DIRS; do
   pushd $subdir
-  if golint | grep -v CannotSetRFMon | grep .; then
+  if golint |
+      grep -v CannotSetRFMon |  # pcap exported error name
+      grep -v DataLost |        # tcpassembly/tcpreader exported error name
+      grep .; then
     exit 1
   fi
   popd
