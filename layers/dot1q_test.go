513aacf0f88021f0b340b01e736614b344768590
Graeme Connell <gconnell@google.com>
2016-10-13 09:47:34 -0600

    Add copyright notice to dot1q test.

--- layers/dot1q_test.go
+++ layers/dot1q_test.go
@@ -1,3 +1,8 @@
+// Copyright 2012 Google, Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
 package layers
 
 import (
