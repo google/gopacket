000188f3a1fb74bd40ea763cac1b6715b55616d6
Paul McMillan <paul@mcmillan.ws>
2017-01-21 20:59:40 -0800

    fix typo in docs
    Fix typo in DecodingLayerParser docs

--- doc.go
+++ doc.go
@@ -288,7 +288,7 @@ the packet's information.  A quick example:
    parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp)
    decoded := []gopacket.LayerType{}
    for packetData := range somehowGetPacketData() {
-     err := parser.DecodeLayers(packetDat, &decoded)
+     err := parser.DecodeLayers(packetData, &decoded)
      for _, layerType := range decoded {
        switch layerType {
          case layers.LayerTypeIPv6:
