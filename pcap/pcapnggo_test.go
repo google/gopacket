86f51361ae0d5ca90252b26b2241df210c548110
Gernot Vormayr <gvormayr@gmail.com>
2018-10-18 22:09:24 +0200

    Fix copyright notice

--- pcap/pcapnggo_test.go
+++ pcap/pcapnggo_test.go
@@ -1,4 +1,8 @@
 // Copyright 2018 The GoPacket Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
 
 package pcap
 
--- pcapgo/doc.go
+++ pcapgo/doc.go
@@ -1,4 +1,8 @@
 // Copyright 2018 The GoPacket Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
 
 /*
 Package pcapgo provides some native PCAP support, not requiring C libpcap to be installed.
