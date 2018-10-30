724fcd62b872cfde69279342ef34a40692bcdaec
Vitor Garcia Graveto <victor.graveto@gmail.com>
2018-10-30 17:20:07 +0000

    Added Author and corrected copyright

--- layers/modbustcp.go
+++ layers/modbustcp.go
@@ -1,4 +1,4 @@
-// Developed by Vitor Graveto
+// Copyright 2018, The GoPacket Authors, All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file in the root of the source
