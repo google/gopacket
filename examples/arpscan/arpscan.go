8f77079ccf2ce88002ad88a5d5fe541ad20f765e
Bo0km4n <pokopenkatuya@gmail.com>
2019-02-23 10:06:53 +0900

    Fix golint warnings

--- examples/arpscan/arpscan.go
+++ examples/arpscan/arpscan.go
@@ -181,8 +181,8 @@ func ips(n *net.IPNet) (out []net.IP) {
 		var buf [4]byte
 		binary.BigEndian.PutUint32(buf[:], num)
 		out = append(out, net.IP(buf[:]))
-		mask += 1
-		num += 1
+		mask++
+		num++
 	}
 	return
 }
