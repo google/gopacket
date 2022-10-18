45e6513685cd572a0c83cf982c165c75d5d6cc4e
Nao Yonashiro <owan.orisano@gmail.com>
2022-10-18 18:41:53 +0900

    docs: fix typo

--- layers/lldp.go
+++ layers/lldp.go
@@ -110,7 +110,7 @@ func (c *LLDPPortID) serializedLen() int {
 }
 
 // LinkLayerDiscovery is a packet layer containing the LinkLayer Discovery Protocol.
-// See http:http://standards.ieee.org/getieee802/download/802.1AB-2009.pdf
+// See http://standards.ieee.org/getieee802/download/802.1AB-2009.pdf
 // ChassisID, PortID and TTL are mandatory TLV's. Other values can be decoded
 // with DecodeValues()
 type LinkLayerDiscovery struct {
