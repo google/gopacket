3fc87e9c745cd47e3bdadfbdd03885a512b1021b
Simeon Miteff <simeon.miteff@gmail.com>
2022-12-07 14:42:31 +1000

    Fix Geneve option flags/length field decoding
    The option header length is 5 bits, not 4, so fix the bit shift for decoding flags, and fix the mask for decoding the length. This also adds a unit test for decoding a max length option (124 byte payload).

--- layers/geneve.go
+++ layers/geneve.go
@@ -60,8 +60,8 @@ func decodeGeneveOption(data []byte, gn *Geneve, df gopacket.DecodeFeedback) (*G
 
 	opt.Class = binary.BigEndian.Uint16(data[0:2])
 	opt.Type = data[2]
-	opt.Flags = data[3] >> 4
-	opt.Length = (data[3]&0xf)*4 + 4
+	opt.Flags = data[3] >> 5
+	opt.Length = (data[3]&0x1f)*4 + 4
 
 	if len(data) < int(opt.Length) {
 		df.SetTruncated()
