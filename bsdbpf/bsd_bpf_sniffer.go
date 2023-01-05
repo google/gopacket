1117a7a78c46d9d99f2b6ffbf7449ca9ba7dd8f9
Doridian <git@doridian.net>
2023-01-04 21:47:59 -0800

    Fix broken SeeSent

--- bsdbpf/bsd_bpf_sniffer.go
+++ bsdbpf/bsd_bpf_sniffer.go
@@ -162,10 +162,10 @@ func NewBPFSniffer(iface string, options *Options) (*BPFSniffer, error) {
 
 	// SeeSent defaults to true, so we change it if false
 	if !sniffer.options.SeeSent {
-		enable := 0
-		_, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(sniffer.fd), uintptr(syscall.BIOCSSEESENT), uintptr(unsafe.Pointer(&enable)))
-		if err != nil {
-			return nil, err
+		var enable int = 0
+		_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(sniffer.fd), uintptr(syscall.BIOCSSEESENT), uintptr(unsafe.Pointer(&enable)))
+		if errno != 0 {
+			return nil, errno
 		}
 	}
 
