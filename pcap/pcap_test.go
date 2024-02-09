bb6d1cf03ca3863850aad2473f398b7ed6c3b1c7
Stefano Balzarotti <stefano.balzarotti@nozominetworks.com>
2024-02-09 17:30:52 +0100

    add signature validation

--- pcap/pcap_test.go
+++ pcap/pcap_test.go
@@ -7,11 +7,13 @@
 package pcap
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/NozomiNetworks/gopacket-fork-nozomi"
@@ -98,9 +100,17 @@ func TestPcapFileRead(t *testing.T) {
 			if file.err != "" {
 				t.Fatalf("Expected error, got none")
 			}
+
 			packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-			for packet := range packetSource.Packets() {
-				packets = append(packets, packet)
+			captureBarrier := sync.WaitGroup{}
+			captureCtx := context.Background()
+			packetChan := make(chan gopacket.Packet, 1000)
+			captureBarrier.Add(1)
+			packetSource.Packets(captureCtx, &captureBarrier, packetChan)
+			captureBarrier.Wait()
+
+			for packetReceived := range packetChan {
+				packets = append(packets, packetReceived)
 			}
 		}
 		if len(packets) != file.num {
